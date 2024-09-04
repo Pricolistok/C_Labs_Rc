160d24a73d0ef48d90a062a19d33cf892488bb8d
Pricolistok <98302043+Pricolistok@users.noreply.github.com>
2024-09-04 21:36:54 +0300

    Delete LABA_3 directory

--- LABA_3/lab_03_01_02/const.h
+++
@@ -1,8 +0,0 @@
-// Заголовочный файл, хранящий в себе значения
-#ifndef CONST_H__
-
-#define CONST_H__
-
-#define N 10
-
-#endif
--- LABA_3/lab_03_02_02/const.h
+++
@@ -1,9 +0,0 @@
-// Заголовочный файл, хранящий в себе значения
-#ifndef CONST_H__
-
-#define CONST_H__
-
-#define N 20
-#define M 10
-
-#endif
