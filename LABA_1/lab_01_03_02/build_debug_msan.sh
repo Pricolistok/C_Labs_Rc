348a8e3923dd4403ff4fb753e3f173ef573f3d64
Pricolistok <98302043+Pricolistok@users.noreply.github.com>
2024-09-04 21:37:05 +0300

    Delete LABA_1 directory

--- LABA_1/lab_01_03_02/build_debug_msan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-to_source=$(dirname "$(readlink -f "$0")")
-cd "$to_source" || exit
-
-clang -std=c99 -Wall -Werror -Wpedantic -Wextra -Wvla -Wfloat-conversion -Wfloat-equal -fsanitize=memory -fPIE -fno-omit-frame-pointer -g -c main.c -o main.o
-clang -g -fsanitize=memory -o app.exe main.o  -pie
