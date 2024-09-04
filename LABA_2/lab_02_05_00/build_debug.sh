4e2bc76b8f2b36d907aeb849f33a4872e3e74e72
Pricolistok <98302043+Pricolistok@users.noreply.github.com>
2024-09-04 21:37:22 +0300

    Delete LABA_2 directory

--- LABA_2/lab_02_05_00/build_debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-to_source=$(dirname "$(readlink -f "$0")")
-cd "$to_source" || exit
-
-gcc -std=c99 -Wall -Werror -Wpedantic -Wextra -Wvla -Wfloat-conversion -Wfloat-equal -g3 -c main.c
-gcc -o app.exe main.c -lm
