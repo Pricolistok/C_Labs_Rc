1629de600673c8005d3c1390eda9b10d99569c2e
Pricolistok <dokolin05@gmail.com>
2024-12-29 18:29:31 +0300

    add unit tests for pop_front

--- lab_10_01_01/unit_tests/pop_front_test.c
+++ lab_10_01_01/unit_tests/pop_front_test.c
@@ -21,6 +21,71 @@ START_TEST(test_one_elem)
 }
 END_TEST
 
+// Тест, когда элемент всего 2
+START_TEST(test_two_elems)
+{
+    int rc;
+    char *name_head_1 = malloc(5 + 1);
+    char *name_head_2 = malloc(4 + 1);
+    char *name_result_1 = malloc(4 + 1);
+    strcpy(name_head_1, "apple");
+    strcpy(name_head_2, "pear");
+    strcpy(name_result_1, "pear");
+    product list_product[] = { { name_head_1, 5, 12}, { name_head_2, 4, 13}};
+    product list_result[] = {{ name_head_2, 4, 13}};
+    node_t *head = NULL;
+    node_t *result = NULL;
+    head = input_to_list(list_product, 2);
+    result = input_to_list(list_result, 1);
+    rc = test_pop_front(head, result);
+    ck_assert_int_eq(rc, OK);
+}
+END_TEST
+
+// Тест, когда элемент
+START_TEST(test_more_elems)
+{
+    int rc;
+    char *name_head_1 = malloc(5 + 1);
+    char *name_head_2 = malloc(4 + 1);
+    char *name_head_3 = malloc(5 + 1);
+    char *name_head_4 = malloc(6 + 1);
+    char *name_result_1 = malloc(4 + 1);
+    char *name_result_2 = malloc(5 + 1);
+    char *name_result_3 = malloc(6 + 1);
+    strcpy(name_head_1, "apple");
+    strcpy(name_head_2, "pear");
+    strcpy(name_head_3, "apple");
+    strcpy(name_head_4, "orange");
+    strcpy(name_result_1, "pear");
+    strcpy(name_result_2, "apple");
+    strcpy(name_result_3, "orange");
+    product list_product[] = { { name_head_1, 5, 12}, {name_head_2, 4, 12},{name_head_3, 5, 12 }, {name_head_4, 6, 42 } };
+    product list_result[] = { {name_result_1, 4, 12},{name_result_2, 5, 12 }, {name_result_3, 6, 42 }};
+    node_t *head = NULL;
+    node_t *result = NULL;
+    head = input_to_list(list_product, 3);
+    result = input_to_list(list_result, 2);
+    rc = test_pop_front(head, result);
+    ck_assert_int_eq(rc, OK);
+}
+END_TEST
+
+// Тест, когда элемент
+START_TEST(test_zero_elems)
+{
+    int rc;
+    char *name_head_1 = malloc(5 + 1);
+    strcpy(name_head_1, "apple");
+    product list_product[] = { { name_head_1, 5, 12} };
+    node_t *head = NULL;
+    node_t *result = NULL;
+    head = input_to_list(list_product, 1);
+    rc = test_pop_front(head, result);
+    ck_assert_int_eq(rc, OK);
+}
+END_TEST
+
 
 
 Suite* pop_front_test(void)
@@ -32,6 +97,9 @@ Suite* pop_front_test(void)
     tc_pos = tcase_create("positive");
 
     tcase_add_test(tc_pos, test_one_elem);
+    tcase_add_test(tc_pos, test_two_elems);
+    tcase_add_test(tc_pos, test_more_elems);
+    tcase_add_test(tc_pos, test_zero_elems);
 
 
     suite_add_tcase(s, tc_pos);
